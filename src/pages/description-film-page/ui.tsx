bd40e3ec8766a066da3adc8eba6b1d1ad2297ee1
yermaka <ermak.080@mail.ru>
2025-07-12 18:35:13 +0800

    add DescriptionFilmPage

--- src/pages/description-film-page/ui.tsx
+++ src/pages/description-film-page/ui.tsx
@@ -1,3 +1,33 @@
-export const DescriptionFilmPage = () => {
-  return <div>DescriptionFilmPage</div>
-}
+import { FilmCard, FilmModel } from "entities/film"
+import { observer } from "mobx-react-lite"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import type { PathParams, ROUTES } from "shared/routes"
+import { ModalError } from "shared/ui/modal-error"
+
+export const DescriptionFilmPage = observer(() => {
+  const params = useParams<PathParams[typeof ROUTES.FILM]>()
+
+  const {
+    store: { filmDescriptionError, filmDescription, getFilmDescriptionById },
+  } = FilmModel
+
+  useEffect(() => {
+    if (params["id"]) getFilmDescriptionById({ id: params["id"] })
+  }, [])
+  if (filmDescriptionError)
+    return <ModalError errorDescription={filmDescriptionError} />
+  if (filmDescription)
+    return (
+      <FilmCard
+        description={filmDescription.description}
+        genres={filmDescription.genres}
+        rating={filmDescription.rating}
+        alternativeName={filmDescription.alternativeName}
+        name={filmDescription.name}
+        year={filmDescription.year}
+        poster={filmDescription.poster}
+        enName={filmDescription.enName}
+      />
+    )
+})
