ee69583e77e6bfea9913de4bd6d28dcec55100cd
yermaka <ermak.080@mail.ru>
2025-07-13 22:19:46 +0800

    add wdiget to film-list-page

--- src/pages/films-list-page/ui.tsx
+++ src/pages/films-list-page/ui.tsx
@@ -1,40 +1,27 @@
-import { FilmModel, FilmRowCard } from "entities/film"
+import { Panel, PanelHeader, Title } from "@vkontakte/vkui"
+import { FilmModel } from "entities/film"
 import { useEffect } from "react"
-import {
-  CardGrid,
-  Group,
-  Panel,
-  PanelHeader,
-  Spinner,
-  Title,
-} from "@vkontakte/vkui"
-import { observer } from "mobx-react-lite"
-import { FilmFilter } from "features/film-filter"
-import { ModalError } from "shared/ui/modal-error"
+
 import { Link } from "react-router"
 import { ROUTES } from "shared/routes"
-import AddFilm from "features/add-film/ui/ui"
+import { CardFilteredList } from "widgets/card-filtered-list"
 
-export const FilmsListPage = observer(() => {
+export const FilmsListPage = () => {
   const {
-    store: { filmListError, getFilms, isLoading, filmList, getGenres, genres },
+    store: { getFilms, getGenres },
   } = FilmModel
 
   useEffect(() => {
     getFilms({
-      "rating.imdb": "5",
-      types: [],
-      year: "2020-2025",
+      "rating.imdb": `0-10`,
+      year: `1990-${new Date().getFullYear()}`,
       limit: 50,
       page: 1,
     })
     getGenres()
   }, [])
 
-  if (filmListError) return <ModalError errorDescription={filmListError} />
-  return isLoading ? (
-    <Spinner />
-  ) : (
+  return (
     <Panel>
       <PanelHeader
         style={{
@@ -52,34 +39,8 @@ export const FilmsListPage = observer(() => {
         >
           Избранное
         </Link>
+        <CardFilteredList />
       </PanelHeader>
-      <FilmFilter
-        genres={genres || []}
-        getOptions={(options) => {
-          console.log("options", options)
-        }}
-      />
-      <Group style={{ display: "flex", justifyContent: "center" }}>
-        <CardGrid
-          padding={true}
-          style={{ width: "80%", alignItems: "stretch" }}
-        >
-          {filmList.docs?.map(
-            ({ id, genres, alternativeName, year, rating, poster }, idx) => (
-              <FilmRowCard
-                key={id}
-                id={id}
-                rating={rating}
-                title={alternativeName}
-                year={year}
-                genres={genres}
-                poster={poster}
-                AddFilm={<AddFilm key={id} film={filmList.docs[idx]} />}
-              />
-            )
-          )}
-        </CardGrid>
-      </Group>
     </Panel>
   )
-})
+}
