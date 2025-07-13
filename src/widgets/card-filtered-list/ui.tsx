2cb9dbc7aedfef7865a32e0a49eb6ef5d57d7de6
yermaka <ermak.080@mail.ru>
2025-07-14 01:32:04 +0800

    update CardFilteredList

--- src/widgets/card-filtered-list/ui.tsx
+++ src/widgets/card-filtered-list/ui.tsx
@@ -4,68 +4,88 @@ import AddFilm from "features/add-film/ui/ui"
 import { FilmFilter } from "features/film-filter"
 import type { FilterOptions } from "features/film-filter/hooks"
 import { observer } from "mobx-react-lite"
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 
 import { ModalError } from "shared/ui/modal-error"
+import { useFetch, useObserver } from "widgets/card-filtered-list/hooks"
 
 export const CardFilteredList = observer(() => {
   const {
-    store: { filmListError, getFilms, isLoading, filmList, genres },
+    store: {
+      filmListError,
+      getFilms,
+      isLoading,
+      filmList,
+      genres,
+      updateFilms,
+    },
   } = FilmModel
-  const [curPage, setCurPage] = useState(1)
+  const [page, setPage] = useState(filmList.page)
   const [gOptions, setOptions] = useState<FilterOptions | null>(null)
-  const endPageRef = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (endPageRef.current) {
-        const { scrollTop, scrollHeight, clientHeight } = endPageRef.current
-        if (scrollTop + clientHeight >= scrollHeight) {
-          setCurPage((prev) => prev + 1)
-        }
-      }
-    }
+  useFetch((options) => {
+    if (options && !isVisible) {
+      const { genres, ratings, years } = options as FilterOptions
 
-    const container = endPageRef.current
-    if (container) {
-      container.addEventListener("scroll", handleScroll)
-    }
-
-    return () => {
-      if (container) {
-        container.removeEventListener("scroll", handleScroll)
-      }
-    }
-  }, [])
-
-  useEffect(() => {
-    if (gOptions) {
-      const { genres, ratings, years } = gOptions
       getFilms({
         "rating.imdb": `${ratings.ratingFrom}-${ratings.ratingTo}`,
         limit: 50,
-        page: curPage,
+        page: 1,
         type: genres.map((g) => g.slug).filter((s) => s !== "any"),
         year: `${years.yearFrom}-${years.yearTo}`,
       })
+      setPage(1)
     }
-  }, [gOptions, curPage])
+  }, gOptions)
+
+  const endPageRef = useRef(null)
+  const { isVisible } = useObserver(endPageRef)
+
+  useFetch((isVisible) => {
+    if (isVisible && gOptions) {
+      const { genres, ratings, years } = gOptions
+
+      updateFilms({
+        "rating.imdb": `${ratings.ratingFrom}-${ratings.ratingTo}`,
+        limit: 50,
+        page: page + 1,
+        type: genres.map((g) => g.slug).filter((s) => s !== "any"),
+        year: `${years.yearFrom}-${years.yearTo}`,
+      })
+      setPage((prev) => prev + 1)
+    }
+  }, isVisible)
 
   if (filmListError) return <ModalError errorDescription={filmListError} />
 
-  return isLoading ? (
-    <Spinner />
-  ) : (
+  return (
     <>
       <FilmFilter
         genres={genres || []}
         getOptions={(options) => {
-          setOptions(options)
+          const { genres, ratings, years } = options
+          const slugs = genres.map((g) => g.slug)
+          const oSlugs = gOptions?.genres.map((g) => g.slug)
+
+          if (
+            ratings.ratingFrom !== gOptions?.ratings.ratingFrom ||
+            ratings.ratingTo !== gOptions?.ratings.ratingTo ||
+            years.yearFrom !== gOptions?.years.yearFrom ||
+            years.yearTo !== gOptions?.years.yearTo ||
+            slugs.sort().join("") !== oSlugs?.sort().join("")
+          )
+            setOptions(options)
         }}
       />
       <Group style={{ display: "flex", justifyContent: "center" }}>
         <CardGrid
-          getRootRef={endPageRef}
+          onScroll={(e) => {
+            console.log(
+              e.currentTarget.clientHeight,
+              window.innerHeight,
+              window.outerHeight
+            )
+          }}
           padding={true}
           style={{ width: "80%", alignItems: "stretch" }}
         >
@@ -83,6 +103,9 @@ export const CardFilteredList = observer(() => {
               />
             )
           )}
+
+          {isLoading && <Spinner />}
+          {<span ref={endPageRef} style={{ height: 1, width: "100%" }}></span>}
         </CardGrid>
       </Group>
     </>
